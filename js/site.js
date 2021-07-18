83ec49cf27662df1ac38da5d2b1f91d2257eb181
Yardley Estiverne <52601743+fyardlest1@users.noreply.github.com>
2021-07-17 21:34:20 -0400

    Initial commit

---
+++ js/site.js
@@ -0,0 +1,9 @@
+// Calling "Hello World" with the constructor: Function
+function helloWorld() {
+    alert('Hello World')
+}
+
+// Calling "Hello World" using Arrow function (=>)
+const helloWorld = () => {
+    alert('Hello World')
+}
