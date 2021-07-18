e90a9955a799c13eba4cc9b93618e5ab6815d20b
fyardlest <fyardlest1@gmail.com>
2021-07-18 11:17:35 -0400

    Update site.js for the template

--- js/site.js
+++ js/site.js
@@ -1,9 +1,18 @@
-// Calling "Hello World" with the constructor: Function
-function helloWorld() {
-    alert('Hello World')
+
+// Get the value from the interface.
+// Start or Controller function or Entry point.
+const getValues = () => {
+    ///
 }
 
-// Calling "Hello World" using Arrow function (=>)
-const helloWorld = () => {
-    alert('Hello World')
+// Generate numbers from the start value to the end value.
+// Our logic function(s)
+const generatenumbers = (start, end) => {
+    //
+}
+
+// Display the even numbers in bold.
+// Display or view functions
+const displayNumbers = () => {
+    //
 }
